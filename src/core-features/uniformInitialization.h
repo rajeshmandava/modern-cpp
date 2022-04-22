9e42f64660ff0d9c8deb149bfa68a442831047c5
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-04-22 21:59:14 +0200

    uniform initialization basic types

---
+++ src/core-features/uniformInitialization.h
@@ -0,0 +1,12 @@
+class uniformInitialization{
+public:
+
+  void builtInTypes();
+  void userDefinedTypes();
+  void arrays();
+  void dynamicArrays();
+  void standardContainers();
+  void userDefinedPODTypes();
+
+
+};
