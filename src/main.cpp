71aaca966d988b18008ff5765d10fcbdd28a4886
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-04-28 21:15:58 +0200

    Range based for loops

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "core-features/useAuto.h"
 #include "core-features/studyVirtualOverride.h"
 #include "core-features/uniformInitialization.h"
+#include "core-features/rangeBasedFor.h"
 #include <iostream>
 
 int main()
@@ -9,13 +10,32 @@ int main()
   // Derived2 derived2Obj;
   // derived2Obj.bar();
   // Derived3 derived3Obj;
-  uniformInitialization obj;
+  // uniformInitialization obj;
   // obj.builtInTypes();
   // obj.arrays();
-  obj.fun({1,2,3}); //using initializer_list for overloading a function
-  obj.fun(5,6,7);
-  
-  
+  // obj.fun({1,2,3}); //using initializer_list for overloading a function
+  // obj.fun(5,6,7);
+  RangeBasedFor obj;
+  std::multimap<int, bool> ans = obj.getRates2();
+  for(auto a:ans)
+    std::cout<<a.first<<" "<<a.second<<std::endl;
+
+  auto iv = obj.getRates();
+  for(auto a:iv)
+    std::cout<<a<<" ";
+  std::cout<<std::endl;
+
+  //compiler deducing the return types in ranged based for
+  std::cout<<"Compiler deducing the types"<<std::endl;
+  for(auto&& a:obj.getRates())
+   {
+    std::cout<<a<<" ";
+
+   }
+   std::cout<<std::endl;
+   std::cout<<"in C++17 Structured binding and decomposition delcaration"<<std::endl;
+   for(auto&& [rate, flag]:obj.getRates2())
+    std::cout<<rate<<" "<<flag<<std::endl;
 }
 
 // $ docker run -it devcafe/cmake-cookbook_ubuntu-18.04
